52618ce1a7a6dc41f6dd5c0738d3016f008783b3
Fırat Işıldak <firatisildak1@gmail.com>
2024-05-23 12:41:18 +0300

    22.05.2024 push

--- blog-app/index.php
+++ blog-app/index.php
@@ -1,34 +1,32 @@
 <?php
-        require "libs/vars.php";
-        require "libs/function.php";
 
-
-        if($_SERVER["REQUEST_METHOD"]=="POST"){
-            $baslik = $_POST["baslik"];
-            $aciklama = $_POST["aciklama"];
-            $resimurl = $_POST["resimurl"];
-
-            filmEkle($filmler,$baslik,$aciklama,$resimurl);
-        }
-
+    require "libs/vars.php";
+    require "libs/functions.php";  
 
 ?>
-    <?php include "views/_header.php";?>
-    <?php include "views/_navbar.php";?>    
-    
-    <div class="container my-5">
-    
-        <div class="row">
-        <?php include "views/_title.php";?>
-            <div class="col-3">
-            <?php include "views/_menu.php";?>
-            </div>
-            <div class="col-9">
-            <?php include "views/_blog-list.php";?>
-            </div>       
+
+<?php include "views/_header.php" ?>
+<?php include "views/_navbar.php" ?>
+
+<div class="container my-3">
+
+    <div class="row">
+
+        <div class="col-3">
+            <?php include "views/_menu.php" ?>     
+
+            
+        </div>
+        <div class="col-9">
+
+            <?php include "views/_title.php" ?>   
+            <?php include "views/_blog-list.php" ?>   
+
         </div>    
+    
     </div>
 
-    <?php include "views/_footer.php"?>
-</body>
-</html>
+</div>
+
+<?php include "views/_footer.php" ?>
+
